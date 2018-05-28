fe6231ad639d11c7505ed3091e507aa974cfb251
Maximiliano Usich <maximilianou@gmail.com>
2018-05-28 10:48:52 -0300

    anda

---
+++ Clase18OOPClasesColecciones/src/pruebas/TestListaZapatillas.java
@@ -0,0 +1,11 @@
+package pruebas;
+
+public class TestListaZapatillas {
+
+    public static void main(String[] args) {
+        System.out.println("[..] test listar zapatillas");
+        
+        System.out.println("[OK] test listar zapatillas");
+    }
+    
+}
