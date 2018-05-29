53a5a92eea6782e190469cfd0a21a98c9b659572
Maximiliano Usich <maximilianou@gmail.com>
2018-05-29 16:34:33 -0300

    comenzando colecciontes

---
+++ Clase18Colectar/src/clase18colectar/Clase18Colectar.java
@@ -0,0 +1,11 @@
+package clase18colectar;
+
+public class Clase18Colectar {
+
+    public static void main(String[] args) {
+        System.out.println("[..] Colecciones!!");
+        
+        System.out.println("[OK] Colecciones!!");
+    }
+    
+}
